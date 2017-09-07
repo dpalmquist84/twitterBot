fb46692b9d547ed99ee85f14b0f87fbe637cedb6
David Palmquist <dpalmquist84@gmail.com>
2017-09-07 11:01:10 -0400

    made a bot that automatically tweets for evertime I'm followed

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'ms0voWiNXfZqVhXxDawH90RNI',
+	consumer_secret: 	'7wHoTNzAEdbigQh9g498W4zjfLglZ066zoGFknfqGSwgtsKwq9',
+	access_token: 	'26042427-44cxKvS8qOyS7qOYA2e9kPXWMdwWr0mN8wgzzBlIe',
+    access_token_secret: 'qCULIf5Xfukl789CLdBO1utahVI5ZoYF1dvztYJbcgbYH'
+}
---
+++ index.js
@@ -0,0 +1,40 @@
+console.log(`bot`)
+
+var Twit = require("twit");
+
+let config = require('./config.js');
+let T = new Twit(config)
+
+
+
+
+let stream = T.stream('user');
+stream.on('follow', followed);
+
+function followed(event){
+  let name = event.source.name;
+  let screenName = event.source.screen_name;
+  tweetIt(`@${screenName} thank you for the follow.`)
+  console.log(`followed`);
+}
+
+
+
+
+
+function tweetIt(txt) {
+
+let tweet = {
+	status: txt
+	}
+	T.post('statuses/update', tweet, tweeted);
+	
+	function tweeted(err, data, response) {
+		if (err) {
+			console.log(`Error ${err}`)
+		} else {
+			console.log(`Worked ${data}`)
+		}
+	}
+}
+
