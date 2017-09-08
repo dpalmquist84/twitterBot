cef43e325ca3b861791dba2a73b455d76a3684c0
David Palmquist <dpalmquist84@gmail.com>
2017-09-08 14:04:56 -0400

    added config.js to .gitignore

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    consumer_key: 'ms0voWiNXfZqVhXxDawH90RNI',
-	consumer_secret: 	'7wHoTNzAEdbigQh9g498W4zjfLglZ066zoGFknfqGSwgtsKwq9',
-	access_token: 	'26042427-44cxKvS8qOyS7qOYA2e9kPXWMdwWr0mN8wgzzBlIe',
-    access_token_secret: 'qCULIf5Xfukl789CLdBO1utahVI5ZoYF1dvztYJbcgbYH'
-}
-
-//changes to test .gitignore
