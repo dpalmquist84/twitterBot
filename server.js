76a44b14329d255c2898913c7494d8fe70348813
David Palmquist <dpalmquist84@gmail.com>
2017-12-15 10:41:43 -0500

    removed unrequired dependencies

--- server.js
+++ server.js
@@ -24,14 +24,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+
 
 // =============================================================================
 // LISTENER
